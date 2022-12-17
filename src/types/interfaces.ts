7563c3381b27b282082ecfe26ae2592802274c2e
Viktoria <aliceinapple99@gmail.com>
2022-12-17 18:25:54 +0300

    init: create enums.ts, interfaces.ts in types folder; create data folder; create data.ts in data folder; add interface, enums and data for product cards

---
+++ src/types/interfaces.ts
@@ -0,0 +1,16 @@
+//Interface for product cards
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  device: string;
+  material: string;
+  color: string;
+  rating: number;
+  stock: number;
+  price: number;
+  mainImage: string;
+  image_1: string;
+  image_2: string;
+}
