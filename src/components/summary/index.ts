ed37359f4ee5e60cc8688adc1a041fcd735e3dc5
YuliaSavchik <yulia.deamchik@gmail.com>
2022-12-21 18:00:28 +0300

    feat: add fanction count products and total sum

--- src/components/summary/index.ts
+++ src/components/summary/index.ts
@@ -1,7 +1,9 @@
 import { createInputs } from "../inputs/index";
 import { createMainButtons } from "../buttons/index";
 
-export function creatSummaryBlock() {
+//function as a parameter takes an array with the prices of the product that are in the cart
+
+export function creatSummaryBlock(pricesCollection: number[]) {
   const summaryContainer: HTMLElement = document.createElement('div');
   summaryContainer.classList.add('summary-container');
 
@@ -12,18 +14,20 @@ export function creatSummaryBlock() {
   const productCount: HTMLElement = document.createElement('p');
   productCount.classList.add('summary-container__product-count');
 
+  const count = countProducts(pricesCollection);
   const productCountNum: HTMLElement = document.createElement('span');
   productCountNum.classList.add('product-count_num');
-  productCountNum.textContent = `${3}`
+  productCountNum.textContent = `${count}`
   productCount.textContent = 'products: ';
   productCount.append(productCountNum);
 
   const total: HTMLElement = document.createElement('p');
   total.classList.add('summary-container__total');
 
+  const sum = countTotalSum(pricesCollection);
   const totalSum: HTMLElement = document.createElement('span');
   totalSum.classList.add('total_sum');
-  totalSum.textContent = `${ 240}$`;
+  totalSum.textContent = `${sum}$`;
   total.innerHTML = 'total: ';
   total.append(totalSum);
 
@@ -35,4 +39,16 @@ export function creatSummaryBlock() {
   summaryContainer.append(title, productCount, total, inputPromoCode, btnBuyNowInCart);
 
   return summaryContainer;
+}
+
+function countProducts(pricesCollection: number[]) {
+  const count: number = pricesCollection.length;
+
+  return count;
+}
+
+function countTotalSum(pricesCollection: number[]) {
+  const count: number = pricesCollection.reduce((acc: number, price: number) => acc + price);
+
+  return count;
 }
