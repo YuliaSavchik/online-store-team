d871da36b8309ec639eac333d82325a515eec4c2
YuliaSavchik <yulia.deamchik@gmail.com>
2022-12-22 12:13:04 +0300

    feat: add html for range slider

---
+++ src/components/noUiSlider/index.ts
@@ -0,0 +1,33 @@
+export function createNoUiSliderBlock(title: string, min: number | string, max: number | string) {
+  const container: HTMLElement = document.createElement('div');
+  container.classList.add('range-slider-block');
+
+  const rangeTitle: HTMLElement = document.createElement('p');
+  rangeTitle.classList.add('range-slider-block__title');
+  rangeTitle.textContent = `${title}`;
+
+  const rangeSlider = createNoUiSlider(min, max);
+  container.append(rangeTitle, rangeSlider);
+
+  return container;
+}
+
+function createNoUiSlider(min: number | string, max: number | string) {
+  const rangeSlider: HTMLElement = document.createElement('div');
+  rangeSlider.classList.add('range-slider');
+
+  const minValue: HTMLElement = document.createElement('div');
+  minValue.classList.add('range-slider__min-value');
+  minValue.textContent = `${min}`;
+
+  const maxValue: HTMLElement = document.createElement('div');
+  maxValue.classList.add('range-slider__max-value');
+  maxValue.textContent = `${max}`;
+
+  const range: HTMLElement = document.createElement('div');
+  range.classList.add('range-slider__range')
+
+  rangeSlider.append(minValue, range, maxValue);
+
+  return rangeSlider;
+}
