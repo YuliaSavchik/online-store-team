1d7cb44a17d4f527a8931a6b764f1580c35aa87d
YuliaSavchik <yulia.deamchik@gmail.com>
2022-12-22 12:34:43 +0300

    feat: add basic style for range slider

--- src/components/noUiSlider/index.ts
+++ src/components/noUiSlider/index.ts
@@ -17,11 +17,13 @@ function createNoUiSlider(min: number | string, max: number | string) {
   rangeSlider.classList.add('range-slider');
 
   const minValue: HTMLElement = document.createElement('div');
-  minValue.classList.add('range-slider__min-value');
+  minValue.classList.add('range-slider__value');
+  minValue.classList.add('value_min');
   minValue.textContent = `${min}`;
 
   const maxValue: HTMLElement = document.createElement('div');
-  maxValue.classList.add('range-slider__max-value');
+  maxValue.classList.add('range-slider__value');
+  maxValue.classList.add('value_max');
   maxValue.textContent = `${max}`;
 
   const range: HTMLElement = document.createElement('div');
